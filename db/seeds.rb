bea93063e5b22bd00e097621d0b16e60636313d8
terence truong <tvn.terence@gmail.com>
2021-11-22 18:07:20 +0100

    seeds puts modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Creating users, bikes and bookings [...]'
-
 # -----------------  PASSWORDS : "secret"  ----------------- #
 
 # 8 users (4 owners and 4 renters)
