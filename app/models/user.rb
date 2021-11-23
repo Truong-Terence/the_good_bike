fba0cb929779c509ec929568edc0fd89ef087ea5
Clement Le Boulanger <hello@clement-leboulanger.com>
2021-11-23 10:53:34 +0100

    Add_has_many_and_routes_owners

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings
+  has_many :bikes
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
