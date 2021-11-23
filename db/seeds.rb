25f8d4a215b08a35e7a89b055223706b9d2e2f47
terence truong <tvn.terence@gmail.com>
2021-11-23 15:50:33 +0100

    seeds with 3pic/bike

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,4 @@
 require "open-uri"
-
-User.destroy
-Bike.destroy
-Booking.destroy
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
