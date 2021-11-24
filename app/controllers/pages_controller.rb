c1a8cfa56945f637ce39e8f75c0afae8734f85d6
Mathias Roux <mathiasroux.pro@gmail.com>
2021-11-24 14:58:37 +0100

    map homepage added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,21 @@
 class PagesController < ApplicationController
+  # CITIES = [paris, lyon, marseille, nantes, bordeaux, montpellier, lille]
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @bikes = Bike.all
+
+    @markers = @bikes.geocoded.map do |bike|
+      {
+        lat: bike.latitude,
+        lng: bike.longitude
+      }
+    end
+  end
+
+  private
+
+  def random_position
+    CITIES.sample(random: Random.new(1))
   end
 end
