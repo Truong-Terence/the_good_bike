2c8811a140b1faac6bbf865baa59b8476e867276
Mathias Roux <mathiasroux.pro@gmail.com>
2021-11-24 15:16:53 +0100

    Revert "map homepage added"
    This reverts commit c1a8cfa56945f637ce39e8f75c0afae8734f85d6.

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -21,7 +21,6 @@ const fitMapToMarkers = (map, markers) => {
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-  const mapElement2 = document.getElementById('map2');
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
