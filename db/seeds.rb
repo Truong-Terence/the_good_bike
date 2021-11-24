fb47f0648eef21840a98b4f319e60757170e6805
terence truong <tvn.terence@gmail.com>
2021-11-24 11:20:30 +0100

    fixed destroy all stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 require "open-uri"
 
+Booking.destroy_all
+Bike.destroy_all
+User.destroy_all
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
