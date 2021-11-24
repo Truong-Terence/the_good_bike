4e0753fc24cb8805f14b914e8171a3d9571869b0
Mathias Roux <mathiasroux.pro@gmail.com>
2021-11-24 15:33:39 +0100

    home map, links search to bikes added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @bikes = Bike.all
+
+    @markers = @bikes.geocoded.map do |bike|
+      {
+        lat: bike.latitude,
+        lng: bike.longitude
+      }
+    end
   end
 end
