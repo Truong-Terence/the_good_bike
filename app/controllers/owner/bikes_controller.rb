82c71f6d9fb2c6af02ba8b8bf271dc3d078e9490
quentin lourenco <quentin.lourenco9@gmail.com>
2021-11-25 12:50:04 +0100

    We can now create a bike

--- app/controllers/owner/bikes_controller.rb
+++ app/controllers/owner/bikes_controller.rb
@@ -4,13 +4,6 @@ class Owner::BikesController < ApplicationController
     @owner_bike = Bike.new
   end
 
-  def create
-    @owner_bike = Bike.new(bike_params)
-    @owner_bike.user = current_user
-    @owner_bike.save!
-    redirect_to owner_bikes_path(@owner_bike)
-  end
-
   def edit
   end
 
@@ -18,11 +11,5 @@ class Owner::BikesController < ApplicationController
   end
 
   def destroy
-  end
-
-  private
-
-  def bike_params
-    params.require(:bike).permit(:name, :description, :address, :size, :kind, :price_per_day, :photos)
   end
 end
